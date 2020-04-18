809071fcbf06c6b3b7af0f9625b59000fc6aa7d8
Dominic <dschroe086@gmail.com>
2020-04-18 15:59:44 -0400

    Updated the map webpage
    After recent discussions with the professor the team has decided to
    change the areas of interest in the project. We are now focusing on
    Dengue in Iquitos, Peru and San Juan, Puerto Rico. As a result, I
    deleted the old geojsons being used.

--- MapData/main.py
+++ MapData/main.py
@@ -9,8 +9,6 @@ from geopy.geocoders import Nominatim
 import folium
 import pandas as pd
 
-ARGENTINA_GEO_DATA = "argentina-provinces.json"
-
 app = Flask(__name__)
 app.add_url_rule('/images/<path:filename>', endpoint='images', view_func=app.send_static_file)
 
@@ -20,13 +18,26 @@ def homepage():
     start_coords = (-22.5, -66.5)
     folium_map = folium.Map(location=start_coords, zoom_start=4)
     
-    arg_data = pd.read_csv(####### INSERT FILE NAME HERE ################)
+    arg_data = pd.read_csv("Enter Csv to read here")
     
     folium.Choropleth(
-        geo_data="ARG_adm1.json",
-        name='choropleth',
-        data=arg_data,
-        columns=['Providences', 'Temperatures'],
+        geo_data="iquitos-peru-geo.geojson",
+        name='Iquitos Temperature',
+        #data=arg_data,
+        #columns=['Providences', 'Temperatures'],
+        key_on='feature.properties.NAME_1',
+        fill_color='YlOrRd',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Temperature (K)',
+        show=False
+    ).add_to(folium_map)
+    
+    folium.Choropleth(
+        geo_data="san-jaun-puerto-rico.geojson",
+        name='San Juan Temperature',
+        #data=arg_data,
+        #columns=['Providences', 'Temperatures'],
         key_on='feature.properties.NAME_1',
         fill_color='YlOrRd',
         fill_opacity=0.7,
